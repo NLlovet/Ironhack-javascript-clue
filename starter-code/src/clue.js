6819ca7404753b57b92a8ae3885a88045ca428ed
Llovet Nicholas <nllovet20199@columbushs.com>
2019-11-13 12:50:52 -0500

    final

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,143 +1,153 @@
 // ITERATION 1
-// ENVELOPE FOR END GAME
-let envelope = {
-    susName: null,
-    susWeapon: null,
-    susRoom: null
-}
+
+
 // Suspects Collection
 const suspectsArray = [
-    {
-        firstName: 'Nurse',
-        lastName: 'White',
-        occupation: 'servant',
-        age: 40,
-        description: 'Works for victim',
-        image: undefined,
-        color: 'red'
-    } ,
-    {
+    mrGreen = {
         firstName: 'Jacob',
         lastName: 'Green',
         occupation: 'Entrepreneur',
+        age: 45,
+        description: 'He has a lot of connections',
+        image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+        color: 'green',
+    } ,
+    drOrchid = {
+        firstName: 'Doctor',
+        lastName: 'Orchid',
+        occupation: 'Scientist',
         age: 26,
-        description: null,
-        image: 'blank',
-        color: 'green'
+        description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+        image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'white',
+        
     } , 
-    {
-        firstName: 'Colonel',
-        lastName: 'Mustard',
-        occupation: 'Army general',
-        age: 31,
-        description: null,
-        image: undefined,
-        color: 'yellow'
-    },
-    {
-        firstName: 'Professor',
+    profPlum = {
+        firstName: 'Victor',
         lastName: 'Plum',
-        occupation: 'Professor',
-        age: 64,
-        description: null,
-        image: undefined,
+        occupation: 'Designer',
+        age: 22,
+        description: 'Billionaire video game designer',
+        image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
         color: 'purple'
     },
-    {
-        firstName: 'Mrs.',
-        lastName: 'Peacock',
-        occupation: 'Baker',
-        age: 42,
-        description: null,
-        image: undefined,
-        color: 'blue'
+    missScarlet = {
+        firstName: 'Kasandra',
+        lastName: 'Scarlet',
+        occupation: 'Actor',
+        age: 31,
+        description: 'She is an A-list movie star with a dark past',
+        image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+        color: 'red'
     },
-    {
-        firstName: 'Mr.',
-        lastName: 'White',
-        occupation: 'Investor',
-        age: 65,
-        description: null,
-        image: undefined,
-        color: 'white'
+    mrsPeacock = {
+        firstName: 'Eleanor',
+        lastName: 'Peacock',
+        occupation: 'Socialité',
+        age: 36,
+        description: 'She is from a wealthy family and uses her status and money to earn popularity',
+        image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+        color: 'blue',
+    },
+    mrMustard = {
+        firstName: 'Jack',
+        lastName: 'Mustard',
+        occupation: 'Retired Football player',
+        age: 62,
+        description: 'He is a former football player who tries to get by on his former glory',
+        image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+        color: 'yellow'
     }
 ];
 
 // Rooms Collection
 const roomsArray = [
-    {room1: 'Kitchen'},
-    {room2: 'Ballroom'},
-    {room3: 'Conservatory'},
-    {room4: 'Dining Room'},
-   { room5: 'Cellar'},
-    {room6: 'Billard Room'},
-    {room7:'Library'},
-    {room8:'Lounge'},
-    {room9:'Hall'},
-    {room10:'Study'},
-    {room11:'Bedroom'},
-    {room12:'Living room'},
-    {room13:'Bathroom'},
-    {room14:'Stairs'},
-    {room15:'Attic'},
+    di = {name: 'Dining Room'},
+    con = {name: 'Conservatory'},
+    ki = {name: 'Kitchen'},
+    st = {name: 'Study'},
+    li = {name: 'Library'},
+    bi = {name: 'Billiard Room'},
+    lo = {name: 'Lounge'},
+    ball = {name: 'Ballroom'},
+    hall = {name: 'Hall'},
+    sp = {name: 'Spa'},
+    livi = {name: 'Living Room'},
+    ob = {name: 'Observatory'},
+    the = {name: 'Theater'},
+    gue = {name: 'Guest House'},
+    pa = {name: 'Patio'}
 ];
 
 // Weapons Collection
 const weaponsArray = [
-    {
-        name: 'Crowbar',
-        weight: 5
-    },
-    {
-        name: 'Axe',
-        weight: 7
-    },
-    {
-        name: 'Knife',
-        weight: 1
-    },
-    {
-        name: 'poker',
-        weight: 3
-    },
-    {
-        name: 'gun',
-        weight: 4
-    },
-    {
-        name: 'screwdriver',
-        weight: .5
-    },
-    {
-        name: 'poison',
-        weight: 0
-    },
-    {
-        name: 'piano',
-        weight: 120
-    },
-    {
-        name: 'sledgehammer',
+    rope = {
+        name: 'rope' ,
         weight: 10
     },
-    {
-        name: 'wire',
+    knife = {
+        name: 'knife',
+        weight: 8
+    },
+    candle = {
+        name: 'candlestick',
         weight: 2
-    }
-
-];
-
-// ITERATION 2
-    //picks random thing
-    function selectRandom (array) {
-    return array[Math.floor(Math.random() * 15)];
-    }
-
-    //makes random cards
-    function pickMystery () {
-    envelope.susName = selectRandom(suspectsArray);
-    envelope.susWeapon = selectRandom(weaponsArray);
-    envelope.susRoom = selectRandom(roomsArray);
-    }
-
-// ITERATION 3
+    },
+    dumbbell = {
+        name: 'dumbbell',
+        weight: 30
+    },
+    poison = {
+        name: 'poison', 
+        weight: 2
+    },
+    axe = {
+        name: 'axe',
+        weight: 15
+    },
+    bat = {
+        name: 'bat',
+        weight: 13
+    },
+    trophy = {
+        name: 'trophy',
+        weight: 25},
+        gun = {
+            name: 'pistol',
+            weight: 20}
+            
+        ];
+        
+        // ITERATION 2
+        
+        //picks random thing
+        
+        function selectRandom (array) {
+            
+            if (array.length == 0) {
+                return undefined;
+            }
+            
+            return array[Math.floor(Math.random() * array.length)];
+        }
+        
+        
+        //makes random cards
+        function pickMystery () {
+            let mystery = {
+                suspect: selectRandom(suspectsArray),
+                weapon: selectRandom(weaponsArray),
+                room: selectRandom(roomsArray)
+            }
+            
+            return mystery;
+        }
+        
+        let envelope = pickMystery();
+        
+        // ITERATION 3
+        
+        function revealMystery (envelope) {
+            
+        return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
+}
